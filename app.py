01869cd8c9e982ff27a6717b7ae73574365efe47
XCoode <jaykumar002kori@gmail.com>
2023-12-07 23:09:47 +0530

    working on it

--- app.py
+++ app.py
@@ -1,62 +1,55 @@
 from flask import Flask, render_template, request
-from urllib.request import urlopen
+from flask_cors import CORS,cross_origin
 from bs4 import BeautifulSoup as bs
+from pymongo import MongoClient
 import requests
+import pandas as pd
 
-print("What you want to search for cars or bike:")
-print("1 for car\n2 for bikes")
-user_input=int(input("Enter your choice:"))
+app = Flask(__name__)
 
-if user_input==1:
-    car_brand=input("Enter the car companie name")
-    #car_model=input("Enter the car model name if any or enter black space")
-    main_url="https://www.zigwheels.com/"+car_brand+"-cars/"
 
-elif user_input==2:
-    bike_brand=input("Enter the bike companie name")
-    #bike_model=input("Enter the bike model name if any or enter black space")
-    main_url="https://www.zigwheels.com/"+bike_brand+"-bikes/"
+client = MongoClient('mongodb+srv://jaykumar002kori:GvlkAZqob1TdfrSz@cluster0.wxxmacs.mongodb.net/?retryWrites=true&w=majority')  
+db = client['vehicals_database']  
+collection = db['vehicals_data'] 
 
-else:
-    print(None)    
+@app.route('/',methods=['GET'])
+@cross_origin()
+def homepage():
+ # Read CSV data into a DataFrame
+# df = pd.read_csv('products.csv', encoding='latin1')
+# Convert DataFrame to list of dictionaries
+#reviews = df.to_dict(orient='records')
+    return render_template('index.html')
 
-response=requests.get(main_url)
+@app.route('/review',methods=['POST','GET']) 
+@cross_origin()
+def index():
+    result_data=None
+    if request.method=='POST':
+        
+        user_query=request.form.get('query')
+        query_type=request.form.get()
+        
+        exists_data=collection.find_one({'query': user_query})
+        
+        if exists_data:
+            result_data=exists_data['data']
+        else:
+            result_data=data_scrap(user_query)
+            collection.update_one({'query': user_query}, {'$set':{"data": result_data}}, upsert=True)
+    return render_template('test.html',result_data=result_data)      
 
-html_source=bs(response.text,'html.parser')
-sorted_html_page=html_source.find('li', {'class':"col-lg-6 txt-c rel modelItem"})
-
-links=[]
-for i in sorted_html_page:
-    links.append(i.find_all('a',{"data-track-label":"launched-model-name"}))
+  
+def data_scrap(query):
+    pass
+    
+    
+    
+    
+if __name__ == '__main__':
+    app.run(debug=True)
        
-combined_links = list(zip(*links))
-#print(combined_links)
-html_content=[]
-for i in combined_links:
-    if type(i)==tuple:
-        for j in i:
-            html_content.append(j)
-html_content_links=[]            
-for i in html_content:
-   html_content_links.append(i['href'])
 
-sorted_link_review=[]
-for review_page in html_content_links:
-    review_page_response=requests.get(review_page)
-    review_page_html=bs(review_page_response.text,"html.parser")
-    sorted_review_page_data=review_page_html.find_all('ul', {'class':"txt-c mt-0 mb-0 fnt-14 mmv-nv"})
-    sorted_link_review.append(sorted_review_page_data)
-
-combined_links_review=list(zip(*sorted_link_review))   
-combined_links_review_list=[]
-
-for k in combined_links_review:
-    if type(k) == tuple:
-        for l in k:
-            combined_links_review_list.append(l)
-combined_links_review_list  
-
-     
 
 
 
